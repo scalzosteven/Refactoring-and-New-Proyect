24c135ed78d690971aa16f659e6d4372f8bee05e
steven <stevemscalzo@grupojuinsa.es>
2020-01-27 15:35:35 +0100

    19: Mover metodo a la clase movie

--- 01-refactoring/src/Movie.php
+++ 01-refactoring/src/Movie.php
@@ -30,4 +30,27 @@ class Movie
     {
         return $this->_title;
     }
+
+    public function obtainChange($daysRented)
+    {
+        $thisAmount = 0;
+        switch ($this->getPriceCode()) {
+            case Movie::REGULAR:
+                $thisAmount += 2;
+                if ($daysRented > 2) {
+                    $thisAmount += ($daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie::NEW_RELEASE:
+                $thisAmount += $daysRented * 3;
+                break;
+            case Movie::CHILDRENS:
+                $thisAmount += 1.5;
+                if ($daysRented > 3) {
+                    $thisAmount += ($daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return $thisAmount;
+    }
 }
--- 01-refactoring/src/Rental.php
+++ 01-refactoring/src/Rental.php
@@ -25,25 +25,7 @@ class Rental
 
     public function obtainChange()
     {
-        $thisAmount = 0;
-        switch ($this->getMovie()->getPriceCode()) {
-            case Movie::REGULAR:
-                $thisAmount += 2;
-                if ($this->getDaysRented() > 2) {
-                    $thisAmount += ($this->getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie::NEW_RELEASE:
-                $thisAmount += $this->getDaysRented() * 3;
-                break;
-            case Movie::CHILDRENS:
-                $thisAmount += 1.5;
-                if ($this->getDaysRented() > 3) {
-                    $thisAmount += ($this->getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return $thisAmount;
+        return $this->getMovie()->obtainChange($this->getDaysRented());
     }
 
     public function calculateFrequentRenterPoints()
