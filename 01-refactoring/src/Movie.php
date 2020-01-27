418dc9109219ef4788e1724dcd51b1effa8d8455
steven <stevemscalzo@grupojuinsa.es>
2020-01-27 18:10:56 +0100

    25:Integramos la clase Prices dentro de Movie, cambiamos el nombre de la directorio prices por Price y renombramos la variable _price

--- 01-refactoring/src/Movie.php
+++ 01-refactoring/src/Movie.php
@@ -2,28 +2,34 @@
 
 namespace Refactoring;
 
+use Refactoring\Price\Children;
+use Refactoring\Price\NewRelease;
+use Refactoring\Price\Price;
+use Refactoring\Price\Regular;
+
 class Movie
 {
     const CHILDRENS = 2;
     const REGULAR = 0;
     const NEW_RELEASE = 1;
     private $_title;
-    private $_priceCode;
+    /**@var  Price*/
+    private $_price;
 
     public function __construct($title, $priceCode)
     {
         $this->_title = $title;
-        $this->_priceCode = $priceCode;
+        $this->setPrice($priceCode);
     }
 
     public function getPriceCode()
     {
-        return $this->_priceCode;
+        return $this->_price->getPriceCode();
     }
 
     public function setPriceCode($arg)
     {
-        $this->_priceCode = $arg;
+        $this->_price = $arg;
     }
 
     public function getTitle()
@@ -68,5 +74,21 @@ class Movie
             $frequentRenterPoints = $frequentRenterPoints + 1;
         }
         return $frequentRenterPoints;
+    }
+
+    private function setPrice($priceCode)
+    {
+        switch ($priceCode){
+            case self::REGULAR:
+                $this->_price = new Regular();
+                break;
+            case self::NEW_RELEASE:
+                $this->_price = new NewRelease();
+                break;
+            case self::CHILDRENS:
+                $this->_price = new Children();
+
+        }
+
     }
 }
