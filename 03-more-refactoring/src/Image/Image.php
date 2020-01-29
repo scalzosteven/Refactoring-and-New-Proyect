ccd89276e910f0b3f7092822818af5df837df248
steven <stevemscalzo@grupojuinsa.es>
2020-01-29 11:27:17 +0100

    06: Extraer metodo obtainImage

--- 03-more-refactoring/src/Image/Image.php
+++ 03-more-refactoring/src/Image/Image.php
@@ -7,26 +7,33 @@ class Image
     {
         $fileDoseNotExist = !file_exists(__DIR__ . '/../../cache/random');
 
-        if ($fileDoseNotExist
-            || time() - filemtime(__DIR__ . '/../../cache/random') > 3) {
-            $responseXml = @file_get_contents(
-                'http://thecatapi.com/api/images/get?format=xml&type=jpg'
-            );
-            if (!$responseXml) {
-                // the cat API is down or something
-                return 'http://cdn.my-cool-website.com/default.jpg';
-            }
-
-            $responseElement = new \SimpleXMLElement($responseXml);
-
-            file_put_contents(
-                __DIR__ . '/../../cache/random',
-                (string)$responseElement->data->images[0]->image->url
-            );
-
-            return (string)$responseElement->data->images[0]->image->url;
+        if ($fileDoseNotExist || time() - filemtime(__DIR__ . '/../../cache/random') > 3) {
+            return $this->obtainImage();
         } else {
             return file_get_contents(__DIR__ . '/../../cache/random');
         }
+    }
+
+    /**
+     * @return string
+     */
+    private function obtainImage(): string
+    {
+        $responseXml = @file_get_contents(
+            'http://thecatapi.com/api/images/get?format=xml&type=jpg' );
+
+        if (!$responseXml) {
+            // the cat API is down or something
+            return 'http://cdn.my-cool-website.com/default.jpg';
+        }
+
+        $responseElement = new \SimpleXMLElement($responseXml);
+
+        file_put_contents(
+            __DIR__ . '/../../cache/random',
+            (string)$responseElement->data->images[0]->image->url
+        );
+
+        return (string)$responseElement->data->images[0]->image->url;
     }
 }
