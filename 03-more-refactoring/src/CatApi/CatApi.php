ac8de9e7a83464d5a13bd4ccbbe5746ad0b78b76
steven <stevemscalzo@grupojuinsa.es>
2020-01-29 10:46:43 +0100

    03: Extraer getImage a una clase Image

--- 03-more-refactoring/src/CatApi/CatApi.php
+++ 03-more-refactoring/src/CatApi/CatApi.php
@@ -2,38 +2,26 @@
 
 namespace Refactoring\CatApi;
 
+use Refactoring\Image\Image;
+
 class CatApi
 {
-    public function getRandomImage()
+    /**@var Image */
+    private $_image;
+
+    public function __construct()
     {
-        return $this->getImagen();
+        $this->setImage();
     }
 
-    /**
-     * @return false|string
-     */
-    private function getImagen()
+    public function getRandomImage()
     {
-        if (!file_exists(__DIR__ . '/../../cache/random')
-            || time() - filemtime(__DIR__ . '/../../cache/random') > 3) {
-            $responseXml = @file_get_contents(
-                'http://thecatapi.com/api/images/get?format=xml&type=jpg'
-            );
-            if (!$responseXml) {
-                // the cat API is down or something
-                return 'http://cdn.my-cool-website.com/default.jpg';
-            }
+        return $this->_image->getImagen();
+    }
 
-            $responseElement = new \SimpleXMLElement($responseXml);
 
-            file_put_contents(
-                __DIR__ . '/../../cache/random',
-                (string)$responseElement->data->images[0]->image->url
-            );
-
-            return (string)$responseElement->data->images[0]->image->url;
-        } else {
-            return file_get_contents(__DIR__ . '/../../cache/random');
-        }
+    private function setImage()
+    {
+        $this->_image = new Image();
     }
 }
