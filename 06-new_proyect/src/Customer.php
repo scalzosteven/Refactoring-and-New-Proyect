7ff7154eaeb01d606da12cd4c4444680ee39f140
steven <stevemscalzo@grupojuinsa.es>
2020-01-30 11:30:38 +0100

    04: Function addTravel

--- 06-new_proyect/src/Customer.php
+++ 06-new_proyect/src/Customer.php
@@ -3,5 +3,18 @@
 
 class Customer
 {
+    private $_name;
+    private $_travels = array();
+
+
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    public function addTravel(Travel $arg)
+    {
+        $this->_travels[] = $arg;
+    }
 
 }
