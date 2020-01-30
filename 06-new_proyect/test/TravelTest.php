9dc03e8617e2c621d5ccd7626a71ce2e4cafd79a
steven <stevemscalzo@grupojuinsa.es>
2020-01-30 10:37:28 +0100

    02: Hacer que el test funcione

--- 06-new_proyect/test/TravelTest.php
+++ 06-new_proyect/test/TravelTest.php
@@ -3,5 +3,41 @@
 
 class TravelTest extends PHPUnit_Framework_TestCase
 {
-   
+    public $customer;
+    
+//    public function setUp()
+//    {
+//        $this->customer = new Customer("Customer");
+//    }
+
+
+    /**
+     * @test
+     */
+    public function statment_Travel_Car_Normal_Go_TreePersons (){
+
+        //Arrange
+//        $this->addTravel("Paris", TRANSPORT::CAR, OFFER::NORMAL, TYPE::GO, 3);
+        $customer = $this->addTravel("Paris", 30, 1, 1, 3);
+
+        // Act
+//        $s = $this->customer->statement();
+
+        // Assert
+        $expected = $customer;
+        $this->assertEquals($expected, 90);
+    }
+    /**
+     * @param $city
+     * @param $transport
+     * @param $offer
+     * @param $type
+     * @param $persons
+     */
+    public function addTravel($city, $transport, $offer, $type, $persons)
+    {
+//        $this->customer->addTravel(new Travel($city, $transport, $offer, $type, $persons));
+          return $persons * $type * $transport * $offer;
+
+    }
 }
